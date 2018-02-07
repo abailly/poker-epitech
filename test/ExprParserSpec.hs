6ee6abfa68d148b0d3327ae0eec0db0a76083b6c
Arnaud Bailly <arnaud.oqube@gmail.com>
2018-02-07 11:53:10 +0100

    evaluate Expr

--- test/ExprParserSpec.hs
+++ test/ExprParserSpec.hs
@@ -4,31 +4,24 @@ import           Data.Char       (isDigit, ord)
 import           Test.Hspec
 import           Test.QuickCheck
 
+type SyntaxError = String
+
+data Expr = Val Int
+          | Plus Expr Expr
+  deriving (Eq, Show)
+
+evalExpr :: Expr -> Int
+evalExpr (Plus v1 v2) = evalExpr v1 + evalExpr v2
+evalExpr (Val v1)     = v1
+
 exprParser :: String -> Either SyntaxError Expr
-exprParser (c1:c2:c3:[]) =
-  -- let e1    = intParser (c1:[])
-  --     eplus = plusParser (c2:[])
-  --     e2    = intParser (c3:[])
-  -- in case e1 of
-  --      Right v1 ->
-  --        case eplus of
-  --          Right plus ->
-  --            case e2 of
-  --              Right v2 -> Right (plus v1 v2)
-  --              Left err -> Left err
-  --          Left err -> Left err
-  --      Left err -> Left err
-
-  do v1   <- intParser (c1:[])
-     plus <- plusParser (c2:[])
-     v2   <- intParser (c3:[])
-
+exprParser [c1]          = intParser [c1]
+exprParser (c1:c2:c3:cs) =
+  do v1   <- intParser  [c1]
+     plus <- plusParser [c2]
+     v2   <- exprParser (c3:cs)
      return $ plus v1 v2
-
-  --           intParser (c1:[])   >>=
-  -- \ v1   -> plusParser (c2:[])  >>=
-  -- \ plus -> intParser (c3:[])   >>=
-  -- \ v2   -> Right (plus v1 v2)
+exprParser _             = Left "syntax error"
 
 
 intParser :: String -> Either SyntaxError Expr
@@ -40,11 +33,6 @@ plusParser :: String -> Either SyntaxError (Expr -> Expr -> Expr)
 plusParser ('+':[]) = Right Plus
 plusParser _        = Left "syntax error"
 
-type SyntaxError = String
-
-data Expr = Val Int
-          | Plus Expr Expr
-  deriving (Eq, Show)
 
 spec :: Spec
 spec = describe "Analyseur syntaxique d'additions à 1 chiffre" $ do
@@ -65,6 +53,15 @@ spec = describe "Analyseur syntaxique d'additions à 1 chiffre" $ do
     it "parse '2+a' est une erreur" $ do
       exprParser "2+a" `shouldBe` Left "syntax error"
 
+    it "parse '2+1+3'" $ do
+      exprParser "2+1+3" `shouldBe` Right (Val 2 `Plus` (Val 1 `Plus` Val 3))
+
+    it "parse '2+1+' est une erreur" $ do
+      exprParser "2+1+" `shouldBe` Left "syntax error"
+
+    it "parse '+' est une erreur" $ do
+      exprParser "+" `shouldBe` Left "syntax error"
+
   describe "Parser de Int" $ do
 
     it "analyse un chiffre comme un entier" $
@@ -82,6 +79,20 @@ spec = describe "Analyseur syntaxique d'additions à 1 chiffre" $ do
     it "analyse le caractère '-' comme une erreur" $
       let Left e = plusParser "-"
       in e `shouldBe` "syntax error"
+
+  describe "Evaluateur arithmétique" $ do
+
+    it "calcule '1+2' retourne 3" $
+      evalExpr (Plus (Val 1) (Val 2)) `shouldBe` 3
+
+    it "calcule '1+3' retourne 4" $
+      evalExpr (Plus (Val 1) (Val 3)) `shouldBe` 4
+
+    it "calcule '1+3+5' retourne 9" $
+      evalExpr (Plus (Val 1) (Plus (Val 3) (Val 5))) `shouldBe` 9
+
+    it "calcule '(1+3)+5' retourne 9" $
+      evalExpr (Plus (Plus (Val 1) (Val 3)) (Val 5)) `shouldBe` 9
 
 newtype Digit = Digit Char
   deriving (Eq, Show)
